c07f87f52a634df6d529e779966cf3ef9b3f268e
Ryan Bourdais <ryanbourdais@outlook.com>
2021-12-18 16:15:36 -0600

    completed project 13 - recreating stripe homepage

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,47 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+import App from "./App";
+import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+  const [location, setLocation] = useState({});
+  const [page, setPage] = useState({ page: "", links: [] });
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link_) => link_.page === text);
+    setPage(page);
+    setLocation(coordinates);
+    setIsSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+  return (
+    <AppContext.Provider
+      value={{
+        isSubmenuOpen,
+        isSidebarOpen,
+        openSubmenu,
+        openSidebar,
+        closeSidebar,
+        closeSubmenu,
+        location,
+        page,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
