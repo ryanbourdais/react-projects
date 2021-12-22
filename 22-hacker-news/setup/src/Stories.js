46dfa1cb5b20cae4e593199cd61e9ca3d228a2f4
Ryan Bourdais <ryanbourdais@outlook.com>
2021-12-22 13:14:44 -0600

    completed project 21- hacker news

--- 22-hacker-news/setup/src/Stories.js
+++ 22-hacker-news/setup/src/Stories.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
 
-import { useGlobalContext } from './context'
+import { useGlobalContext } from "./context";
 
 const Stories = () => {
-  return <h2>stories component</h2>
-}
+  const { isLoading, hits, removeStory } = useGlobalContext();
 
-export default Stories
+  if (isLoading) {
+    return <div className="loading" />;
+  }
+  return (
+    <section className="stories">
+      {hits.map((hit) => {
+        const { author, title, url, objectID: id, num_comments, points } = hit;
+        return (
+          <article key={id} className="story">
+            <h4 className="title">{title}</h4>
+            <p className="info">
+              {points} points by <span>{author} | </span>
+              {num_comments} comments
+            </p>
+            <div>
+              <a
+                href={url}
+                className="read-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Read More
+              </a>
+              <button className="remove-btn" onClick={() => removeStory(id)}>
+                remove
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </section>
+  );
+};
+
+export default Stories;
--- 22-hacker-news/setup/src/reducer.js
+++ 22-hacker-news/setup/src/reducer.js
@@ -4,7 +4,48 @@ import {
   REMOVE_STORY,
   HANDLE_PAGE,
   HANDLE_SEARCH,
-} from './actions'
+} from "./actions";
 
-const reducer = () => {}
-export default reducer
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_LOADING:
+      return { ...state, isLoading: true };
+    case SET_STORIES:
+      return {
+        ...state,
+        isLoading: false,
+        hits: action.payload.hits,
+        nbPages: action.payload.nbPages,
+      };
+    case REMOVE_STORY:
+      return {
+        ...state,
+        hits: state.hits.filter((story) => story.objectID !== action.payload),
+      };
+    case HANDLE_SEARCH:
+      return {
+        ...state,
+        query: action.payload,
+        page: 0,
+      };
+    case HANDLE_PAGE:
+      if (action.payload === "dec") {
+        let tempPage = state.page - 1;
+        if (tempPage < 0) {
+          tempPage = state.nbPages - 1;
+        }
+        return { ...state, page: tempPage };
+      }
+      if (action.payload === "inc") {
+        let tempPage = state.page + 1;
+        if (tempPage == state.nbPages) {
+          tempPage = 0;
+        }
+        return { ...state, page: tempPage };
+      }
+      break;
+    default:
+      throw new Error(`no matching "${action.type}" action type`);
+  }
+};
+export default reducer;
